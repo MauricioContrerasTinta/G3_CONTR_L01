b345b61240635367d89798eaf5a4119075f60f52
MauricioContrerasTinta <contrerastinta@gmail.com>
2025-03-19 19:22:11 -0500

    coordeanda edit

--- coordenada.java
+++ coordenada.java
@@ -2,5 +2,19 @@ import java.util.*;
 public class Coordenada{
     private double x;
     private double y;
-        
+    
+    public Coordenada(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Coordenada(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordenada(Coordenada c) {
+        this.x = c.x;
+        this.y = c.y;
+    }
 }
