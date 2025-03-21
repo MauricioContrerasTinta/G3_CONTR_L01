a915814037bdbc1dae9b58f1a3ae0094cb820976
MauricioContrerasTinta <165443093+MauricioContrerasTinta@users.noreply.github.com>
2025-03-20 19:56:09 -0500

    Update Verificador.java

--- Verificador.java
+++ Verificador.java
@@ -1,6 +1,13 @@
 public class Verificador {
     public static boolean seSobreponen(Rectangulo a, Rectangulo b){
-        return (a.getEsquina2().getX()) <= b.getEsquina1().getX()
+        return (a.getEsquina2().getX()) <= b.getEsquina1().getX() || a.getEsquina1().getX() >= b.getEsquina2().getX() || a.getEsquina2().getY() <= b.getEsquina1().getY() || a.getEsquina1().getY() >= b.getEsquina2().getY());
+    }
 
+    public static boolean estanJuntos(Rectangulo a, Rectangulo b){
+        return (a.getEsquina2().getX() == b.getEsquina1().getX() || a.getEsquina1().getX() == b.getEsquina2().getX() || a.getEsquina2().getY() == b.getEsquina1().getY() || a.getEsquina1().getY() == b.getEsquina2().getY());
+    }
+
+     public static boolean sonDisjuntos(Rectangulo a, Rectangulo b) {
+        return !seSobreponen(a, b) && !estanJuntos(a, b);
     }
 }
